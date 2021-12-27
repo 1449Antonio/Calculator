2e3cfa493cda07513684666f5ad1d4a3561fe4df
1449Antonio <49537223+1449Antonio@users.noreply.github.com>
2021-12-27 14:16:03 -0500

    Create Menu.js

---
+++ src/Components/Menu/Menu.js
@@ -0,0 +1,11 @@
+function Menu(props) {
+    return (
+      <div className="Menu">
+          <button>Help</button>
+          <button style = {{margin: '10px'}}>About Calculator</button>
+          <button>Clear History</button>
+      </div>
+    );
+  }
+  
+  export default Menu;
