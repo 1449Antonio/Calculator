fe88c861b8809e9668a05610ad6d6e5fa2c99083
1449Antonio <49537223+1449Antonio@users.noreply.github.com>
2021-12-28 13:44:30 -0500

    Update History.js

--- src/Components/History/History.js
+++ src/Components/History/History.js
@@ -1,17 +1,43 @@
 import './History.css';
 import React, {useState, useEffect} from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 
 function History(props) {
     const[currentHistory, setCurrentHistory] = useState('');
+    const[showHistory, setShowHistory] = useState(false)
+    const[dropDown, setDropDown] = useState(false)
+    const[operandHistory, setOperandHistory] = useState('')
     
+    const showOperands = () =>{
+      if (dropDown === false)
+        setDropDown(true)
+      else
+        setDropDown(false)
+    }
+
     useEffect(()=>{
         if (props.value !== '')
-             setCurrentHistory(props.value)
+        {
+          setCurrentHistory(props.value)
+          setShowHistory(true)
+        }
+        if (props.operandHist !== '')
+          setOperandHistory(props.operandHist)
     }, [props])
+
     return (
+
       <div className="History">
-        <p>{currentHistory}</p>
+        {
+        (showHistory === true) ? <p style = {{margin: '0px'}}>{currentHistory}<button style = {{cursor: 'pointer', width: '20px'}} onClick = {showOperands}><FontAwesomeIcon icon = {faAngleDown}></FontAwesomeIcon></button></p>
+        : <p>{currentHistory}</p>
+        }
+
+        {
+        (dropDown === true) ? <p style = {{backgroundColor: 'white', border: '1px solid black', height: '20px', fontSize: '100%', margin: '0px', width: '30px', position: 'absolute', left: '48.5%'}}>{operandHistory}</p>
+        : console.log('')
+        }
       </div>
     );
   }
