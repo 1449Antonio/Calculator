80dafbd5524db27fd54691112add1a2180f9c115
1449Antonio <49537223+1449Antonio@users.noreply.github.com>
2022-01-03 02:36:46 -0500

    Update HistoryEntry.js

--- src/Components/History/HistoryEntry/HistoryEntry.js
+++ src/Components/History/HistoryEntry/HistoryEntry.js
@@ -10,8 +10,8 @@ function HistoryEntry(props) {
       }
     return (
     <div className="HistoryEntry">
-        <p style = {{margin: '0px', position: 'relative'}}>{props.operHistory.value}<button style = {{cursor: 'pointer', width: '20px'}} onClick = {showOperands}><FontAwesomeIcon icon = {faAngleDown}></FontAwesomeIcon></button></p>
-        {
+        <div style = {{margin: '0px', position: 'relative'}}><div style={{display: 'inline-block'}} onClick = {()=>props.click(props, props.num)}>{props.operHistory.value}</div><button style = {{cursor: 'pointer', width: '20px', display: 'inline-block'}} onClick = {showOperands}><FontAwesomeIcon icon = {faAngleDown}></FontAwesomeIcon></button></div>
+        { 
             (dropDown === true) ? <p style = {{backgroundColor: 'white', border: '1px solid black', height: '20px', fontSize: '100%', margin: '0px', position: 'relative', left: '0%', display: 'inline-block'}}>{props.operHistory.operation}</p>
             : null
         }
